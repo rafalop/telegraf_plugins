0a0051329401cf244bc29b73fccb06b93de2a970
Rafael Lopez <rafael.lopez@monash.edu>
2020-10-19 16:00:44 +1100

    fixed code

--- dircount/plugins/input/dircount/dircount.go
+++ dircount/plugins/input/dircount/dircount.go
@@ -2,12 +2,63 @@ package main
 
 import(
     "os"
+    "log"
+    "github.com/influxdata/telegraf"
+    "github.com/influxdata/telegraf/plugins/inputs"
 )
 
-func main() {
-    dir_open, _ := os.Open(`/home`)
-    entries, err := dir_open.Readdirnames(-1)
-    if err != nil{
-        println(entries)
+type dirCount struct{
+  Directories []string `toml:"directories"`
+  ValueName string `toml:"value_name"`
+}
+
+func init() {
+  inputs.Add("dircount", func() telegraf.Input {
+    return &dirCount{
+      Directories: []string{},
+      ValueName: "entries",
     }
+  })
+}
+
+func (d *dirCount) Description() string {
+  return "Return number of entries in a directory."
+}
+
+func (d *dirCount) SampleConfig() string {
+  return `
+  ## Grabs count of entries in a dir (non recursive)
+  [inputs.dircount]
+  # List of directories you want to count entries for
+  # directories = ["/home", "/var/log"]
+  # name for metric
+  # value_name = "entries"
+`
+}
+
+func (d *dirCount) Gather(a telegraf.Accumulator) error {
+  d.sendMetric(a)
+  return nil
+}
+
+func (d *dirCount) getCount(path string) uint64 {
+  dir_open, _ := os.Open(path)
+  entries, err := dir_open.Readdirnames(-1)
+  if err != nil{
+    log.Println(err, ` :`,path)
+  }
+  count := uint64(len(entries))
+  return count
+}
+
+func (d *dirCount) sendMetric(a telegraf.Accumulator) {
+  for _, dir := range d.Directories {
+    a.AddFields("dircount", 
+                map[string]interface{}{
+                  "dir": dir,
+                  d.ValueName : d.getCount(dir),
+                },
+                nil,
+    )
+  }
 }
